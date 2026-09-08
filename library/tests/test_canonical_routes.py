0f118110254dea1165ab7057052b6cb3e6a3bb18
Abdul Kabeer <144008371+a-kabeer@users.noreply.github.com>
2026-09-08 22:47:56 +0500

    Add regression tests for canonical route redirects

---
+++ library/tests/test_canonical_routes.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from .helpers import make_user
+
+
+class CanonicalRouteTests(TestCase):
+    """Legacy entry points redirect instead of duplicating view bindings."""
+
+    def setUp(self):
+        make_user(username="route_admin", password="pass12345", role="Admin")
+        self.client.login(username="route_admin", password="pass12345")
+
+    def test_dashboard_alias_redirects_to_canonical_home(self):
+        response = self.client.get(reverse("dashboard"))
+
+        self.assertRedirects(
+            response,
+            reverse("library_home"),
+            status_code=301,
+        )
+
+    def test_loan_add_alias_redirects_to_circulation_issue(self):
+        response = self.client.get(reverse("loan_add"))
+
+        self.assertRedirects(
+            response,
+            reverse("circulation_issue"),
+            status_code=301,
+        )
+
+    def test_active_loans_alias_redirects_to_filtered_loan_list(self):
+        response = self.client.get(reverse("circulation_active_loans"))
+
+        self.assertRedirects(
+            response,
+            "%s?status=active" % reverse("loan_list"),
+            status_code=301,
+        )
